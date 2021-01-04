a0afdaf52f8653aba741146a7feecba3a4e602af
seabbs <s.e.abbott12@gmail.com>
2021-01-04 19:12:14 +0000

    add missing loo compare back in

--- R/combine_data.r
+++ R/combine_data.r
@@ -14,8 +14,9 @@ mobility <- readRDS(here("data", "mobility.rds"))
 # Combine data sources ----------------------------------------------------
 ltla_rt <- sgene_by_ltla %>%
   inner_join(rt_weekly, by = c("week_infection", "ltla_name")) %>%
-  select(week_infection, nhser_name, ltla_name, ltla_code, prop_variant,
-         prop_variant_sd, samples, cases, rt_mean = mean, rt_sd = sd) %>%
+  select(week_infection, generation_time, nhser_name, ltla_name, ltla_code,
+         prop_variant, prop_variant_sd, samples, cases, rt_mean = mean, 
+         rt_sd = sd) %>%
   mutate(time = as.numeric((week_infection - min(week_infection)) / 7))
 
 ltla_rt_with_covariates <- ltla_rt %>%
