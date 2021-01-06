9dc81ec3558c2b5cc915bcf50d78af565b727de0
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2021-01-06 16:31:39 +0000

    interpolate all mobility / tier data

--- R/combine_data.r
+++ R/combine_data.r
@@ -18,6 +18,11 @@ utla_rt <- sgene_by_utla %>%
          prop_variant, samples, cases, rt_mean = mean) %>%
   mutate(time = as.numeric((week_infection - min(week_infection)) / 7))
 
+tiers <- tiers %>%
+	complete(week_infection = unique(utla_rt$week_infection),
+		 utla_name = unique(utla_name)) %>%
+	replace_na(list(tier = "none"))
+
 utla_rt_with_covariates <- utla_rt %>%
   inner_join(tiers, by = c("week_infection", "utla_name")) %>%
   inner_join(mobility, by = c("week_infection", "utla_name"))
