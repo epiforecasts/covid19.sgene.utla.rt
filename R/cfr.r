164d7d92dc21ae78a57fa583cb0ace56ec7b11b3
seabbs <s.e.abbott12@gmail.com>
2021-01-07 18:29:50 +0000

    initial working implementation

--- R/cfr.r
+++ R/cfr.r
@@ -6,6 +6,8 @@ library(vroom)
 library(lubridate)
 library(brms)
 
+options(mc.cores = 4)
+
 # Data --------------------------------------------------------------------
 # get raw cases by data of infection from epiforecasts.io 
 case_infs <- vroom(paste0("https://raw.githubusercontent.com/epiforecasts/covid-rt-estimates/",
@@ -29,7 +31,8 @@ infections <- case_infs %>%
 
 # Define covariates -------------------------------------------------------
 # get variant proportion
-sgene_by_utla <- readRDS(here("data", "sgene_by_utla.rds"))
+sgene_by_utla <- readRDS(here("data", "sgene_by_utla.rds")) %>% 
+  drop_na(prop_variant)
 
 # make infections weekly summary
 weekly_infections <- infections %>% 
@@ -52,45 +55,77 @@ deaths_with_cov <- deaths_with_cov %>%
          time = time - min(time),
          time = (time - mean(time)) / sd(time))
 
-# Define models -----------------------------------------------------------
-
+# Define model ------------------------------------------------------------
+# define custom negative binomial family including variant factor and cases
 variant_nb <- custom_family(
   "variant_nb", dpars = c("mu", "phi", "alpha"),
-  links = c("logit", "identity", "identity"),
+  links = c("logit", "log", "identity"),
   lb = c(0, 0, 0),
-  type = "real",
-  vars = "vreal1[n]"
+  type = "int",
+  vars = c("f[n]", "cases[n]")
 )
-
-stan_funs <- "
-real variant_nb_lpdf(int y, real mu, real phi, real alpha,
-                     int cases) {
-    real scaled_cases = (1 + (alpha - 1)) * mu * cases;
+# define stan code to scale cfr by cases and variant fraction
+make_stanvars <- function(data) {
+  stan_funs <- "
+real variant_nb_lpmf(int y, real mu, real phi, real alpha,
+                     real f, int cases) {
+    real scaled_cases = (1 + (alpha - 1) * f) * mu * cases;
     real sqrt_phi = 1 / sqrt(phi);
     return  neg_binomial_2_lpmf(y | scaled_cases, sqrt_phi);
                             }
 real variant_nb_rng(int y, real mu, real phi, real alpha,
-                     int cases) {
-    real scaled_cases = (1 + (alpha - 1)) * mu * cases;
-    real sqrt_phi = 1 / sqrt(phi);
-    return  neg_binomial_2_rng(y | scaled_cases, sqrt_phi);
+                    real f, int cases) {
+    real scaled_cases = (1 + (alpha - 1) * f) * mu * cases;
+    return  neg_binomial_2_rng(scaled_cases, phi);
                             }
 "
-stanvars <- stanvar(block = "functions", scode = stan_funs)
-
-# Set up shared priors ----------------------------------------------------
+  stanvars <- c(stanvar(block = "functions", scode = stan_funs),
+                stanvar(block = "data",
+                        scode = "  real f[N];",
+                        x = data$prop_variant,
+                        name = "f"),
+                stanvar(block = "data",
+                        scode = "  int cases[N];",
+                        x = data$cases,
+                        name = "cases")
+  )
+  return(stanvars)
+}
+# define priors
 priors <- c(prior(lognormal(0, 1), class = alpha))
 
-# Fit models --------------------------------------------------------------
-
-base_model <- function(form, iter = 2000, ...) {
+# define model function
+nb_model <- function(form, iter = 1500, data = deaths_with_cov, ...) {
+  message("Fitting: ", form)
   brm(formula = form,
       family = variant_nb,
-      stanvars = stanvars, 
+      prior = priors,
+      data,
+      stanvars = make_stanvars(data),
+      control = list(adapt_delta = 0.9),
       warmup = 500, iter = iter, ...)
 }
+# Fit models --------------------------------------------------------------
+fits <- list()
+# define models to fit
+fits[["intercept"]] <- as.formula(deaths ~ 1)
+fits[["cases"]] <- as.formula(deaths ~ normalised_cases)
+fits[["region"]] <- as.formula(deaths ~ region)
+fits[["time"]] <- as.formula(deaths ~ s(time, k = 9))
+fits[["utla"]] <- as.formula(deaths ~ (1 | utla))
+fits[["all"]] <- as.formula(deaths ~ normalised_cases + s(time, k = 9) + region + (1 | utla))
 
-base_model(deaths | vreal(prop_variant) ~ 1)
+#fit models
+fits <- lapply(fits, nb_model)
+
+# Variant effect ----------------------------------------------------------
+variant_effect <- lapply(fits,  function(x) {
+  samples <- posterior_samples(x, "alpha")
+  q <- quantile(samples[, "alpha"] - 1, c(0.025, 0.5, 0.975))
+  q <- round(q, 2)
+  return(q)
+})
+names(variant_effect) <- names(fits)
+
 # Compare models ----------------------------------------------------------
-
-
+# requires custom log_lik functions to be implemented for variant_nb family
