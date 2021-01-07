eb45c94aa6b8c8d8c4df26b32f63cd3651c45890
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2021-01-07 16:42:38 +0000

    data update

--- R/extract_public_data.r
+++ R/extract_public_data.r
@@ -10,6 +10,32 @@ library(janitor)
 # Extract data ---------------------------------------------------
 week_start <- 1
 
+utlas <- readRDS(here("data", "sgene_by_utla.rds")) %>%
+  .$utla_name %>%
+  unique()
+
+baseline <- tibble(
+  date = c(as.Date("2020-10-05")),
+  utla_name = utlas,
+  tier = "none"
+)
+
+tier_1 <- tibble(
+  date = c(as.Date("2020-10-12")),
+  utla_name = utlas,
+  tier_1 = "tier_1"
+)
+
+initial_tiers <- read_csv(here("data-raw", "tiers", "initial_tiers.csv")) %>%
+  full_join(tier_1, by = c("date", "utla_name")) %>%
+  mutate(tier = if_else(is.na(tier), tier_1, tier)) %>%
+  select(-tier_1) %>%
+  arrange(utla_name, date)
+
+start_tiers <- baseline %>%
+  bind_rows(initial_tiers) %>%
+  mutate(date = as.Date(date))
+
 old_tier_files <-
   list.files(here("data-raw", "tiers"), "uk_tier_data_parliament")
 
@@ -33,25 +59,34 @@ old_tiers <- lapply(old_tier_files, function(x) {
 ltla_utla <- old_tiers %>%
   filter(!is.na(utla)) %>%
   select(laname, match_utla = utla) %>%
-  distinct()
+ distinct()
 
 old_tiers <- old_tiers %>%
   filter(country == "E") %>%
+  filter(date < "2020-11-05") %>%
   mutate(tier = if_else(tier %in% c("Medium", "1"), "tier_1", tier),
          tier = if_else(tier %in% c("High", "2"), "tier_2", tier),
-         tier = if_else(tier %in% c("Very High", "3"), "tier_3", tier),
-         tier = if_else(date > "2020-11-04", "national_lockdown", tier)) %>%
+         tier = if_else(tier %in% c("Very High", "3"), "tier_3", tier)) %>%
   left_join(ltla_utla, by = "laname") %>%
   mutate(utla = if_else(is.na(utla), match_utla, utla)) %>%
   select(-match_utla) %>%
-  complete(date = seq(min(date), as.Date("2020-12-01"), by = "day"),
-           utla = unique(utla)) %>%
-  arrange(utla, date) %>%
-  group_by(utla) %>%
-  mutate(tier = if_else(is.na(tier) & date == min(date), "tier_1", tier)) %>%
-  fill(tier, .direction = "down") %>%
   filter(!is.na(utla)) %>%
-  select(date, utla, tier)
+  mutate(utla = if_else(utla %in% c("Cornwall", "Isles of Scilly"),
+                        "Cornwall and Isles of Scilly", utla),
+         utla = if_else(utla %in% c("Hackney", "City of London"),
+                        "Hackney and City of London", utla)) %>%
+  group_by(date, utla_name = utla, tier) %>%
+  summarise(n = n(), .groups = "drop") %>%
+  group_by(date, utla_name) %>%
+  filter(n == max(n)) %>%
+  ungroup() %>%
+  select(-n)
+
+national_lockdown <- tibble(
+  date = c(as.Date("2020-11-05")),
+  utla_name = utlas,
+  tier = "national_lockdown_nov"
+)
 
 # Extract recent tiers ----------------------------------------------------
 new_tier_files <-
@@ -65,29 +100,36 @@ new_tiers <- lapply(new_tier_files, function(x) {
 }) %>%
   bind_rows() %>%
   clean_names() %>%
-  select(date, tier, utla = ctyua20nm, nhs = rgn19nm)
-
+  select(date, tier, utla_name = ctyua20nm, nhs = rgn19nm) %>%
+  mutate(utla_name = if_else(utla_name %in% c("Cornwall", "Isles of Scilly"),
+                             "Cornwall and Isles of Scilly", utla_name),
+         utla_name = if_else(utla_name %in% c("Hackney", "City of London"),
+                             "Hackney and City of London", utla_name)) %>%
+  group_by(date, nhs, utla_name, tier) %>%
+  summarise(n = n(), .groups = "drop") %>%
+  group_by(date, nhs, utla_name) %>%
+  filter(n == max(n)) %>%
+  ungroup() %>%
+  select(-n)
 
 utla_nhs <- new_tiers %>%
-  select(utla, nhs) %>%
+  select(utla_name, nhs) %>%
   distinct()
 
 new_tiers <- new_tiers %>%
   select(-nhs) %>%
   mutate(tier = if_else(tier == "National Lockdown", "national_lockdown",
-                        paste0("tier_", tier))) %>%
-  complete(date = seq(min(date), today(), by = "day"), utla = unique(utla)) %>%
-  arrange(utla, date) %>%
-  fill(tier, .direction = "down")
+                        paste0("tier_", tier)))
 
 # Join into single dataset ------------------------------------------------
-tiers <- bind_rows(old_tiers, new_tiers) %>%
-  rename(utla_name = utla) %>%
+tiers <- start_tiers %>%
+  bind_rows(old_tiers) %>%
+  bind_rows(national_lockdown) %>%
+  bind_rows(new_tiers) %>%
+  complete(date = seq(min(date), today(), by = "day"),
+           utla_name = unique(utla_name)) %>%
   arrange(utla_name, date) %>%
-  mutate(utla_name = if_else(utla_name %in% c("Cornwall", "Isles of Scilly"),
-                             "Cornwall and Isles of Scilly", utla_name),
-         utla_name = if_else(utla_name %in% c("Hackney", "City of London"),
-                             "Hackney and City of London", utla_name)) %>%
+  fill(tier, .direction = "down") %>%
   mutate(week_infection = floor_date(date, "week", week_start)) %>%
   group_by(utla_name, week_infection, tier) %>%
   summarise(n = n(), .groups = "drop") %>%
@@ -105,10 +147,11 @@ mobility <- read_csv(mobility_file) %>%
   select(date, laname = lad_nm,
          variable = variable, value = value) %>%
   inner_join(ltla_utla, by = "laname") %>%
-  rename(utla = match_utla) %>%
-  right_join(utla_nhs, by = c("utla")) %>%
-  complete(date = seq(min(date, na.rm = TRUE), max(date, na.rm = TRUE), by = "day"), 
-	   utla = unique(utla), 
+  rename(utla_name = match_utla) %>%
+  right_join(utla_nhs, by = c("utla_name")) %>%
+  complete(date = seq(min(date, na.rm = TRUE),
+                      max(date, na.rm = TRUE), by = "day"),
+	   utla_name = unique(utla_name),
 	   variable = unique(variable)) %>%
   filter(!is.na(date), !is.na(variable)) %>%
   group_by(date, nhs, variable) %>%
@@ -117,12 +160,12 @@ mobility <- read_csv(mobility_file) %>%
   mutate(value = if_else(is.na(value), median, value)) %>%
   fill(value, .direction = "downup") %>%
   select(-nhs, -median) %>%
-  mutate(utla = if_else(utla %in% c("Cornwall", "Isles of Scilly"),
-                        "Cornwall and Isles of Scilly", utla),
-         utla = if_else(utla %in% c("Hackney", "City of London"),
-                        "Hackney and City of London", utla)) %>%
+  mutate(utla_name = if_else(utla_name %in% c("Cornwall", "Isles of Scilly"),
+                             "Cornwall and Isles of Scilly", utla_name),
+         utla_name = if_else(utla_name %in% c("Hackney", "City of London"),
+                        "Hackney and City of London", utla_name)) %>%
   mutate(week_infection = floor_date(date, "week", week_start)) %>%
-  group_by(week_infection, utla_name = utla, variable) %>%
+  group_by(week_infection, utla_name, variable) %>%
   summarise(value = mean(value), .groups = "drop") %>%
   group_by(variable) %>%
   mutate(value = (value - mean(value, na.rm = TRUE)) /
