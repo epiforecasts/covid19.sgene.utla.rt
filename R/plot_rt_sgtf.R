eee1130afd113dd31f83f888789c2a9af41a6369
seabbs <s.e.abbott12@gmail.com>
2021-01-14 21:32:20 +0000

    updates

--- R/plot_rt_sgtf.R
+++ R/plot_rt_sgtf.R
@@ -1,6 +1,8 @@
 # Packages ----------------------------------------------------------------
 library(here)
+library(vroom)
 library(ggplot2)
+library(lemon)
 library(dplyr)
 library(cowplot)
 library(lubridate)
@@ -8,7 +10,7 @@ library(lubridate)
 library(covidregionaldata)
 
 # Get data ----------------------------------------------------------------
-utla_rt <- readRDS(here("data", "utla_rt_with_covariates.rds"))
+utla_rt <- readRDS(url("https://raw.github.com/epiforecasts/covid19.sgene.utla.rt/main/data/utla_rt_with_covariates.rds"))
 
 # link with cases
 cases <- get_regional_data("UK", include_level_2_regions = TRUE) %>%
@@ -30,25 +32,25 @@ utla_rt  <- utla_rt_cases %>%
 # filter for target dates
 target_rt <- utla_rt %>% 
   filter(as.character(week_infection) %in% c("2020-10-26", "2020-11-09", 
-                               "2020-11-23", "2020-12-07",
-                               "2020-12-21"))
+                                             "2020-11-23", "2020-12-07",
+                                             "2020-12-21"))
 
 plot <- ggplot(target_rt, aes(x = prop_sgtf, y = rt_mean,
-                      fill = nhser_name, size = cases)) +
-               geom_jitter(pch = 21) +
-               facet_wrap(. ~ week_infection, ncol = 2) +
-               scale_fill_brewer("", palette = "Set1") +
-               xlab("Proportion SGTF") +
-               ylab("Mean reproduction number") +
-               theme_cowplot(font_size = 10) +
-               geom_hline(yintercept = 1, linetype = "dashed") +
-               labs(size = paste("Cases")) +
-               guides(fill = guide_legend(title = "NHS region", ncol = 1), 
-               size = guide_legend(ncol = 1)) +
-               theme(legend.position = c(0.7, 0.125),
-                     legend.direction = "vertical",
-                     legend.box = "horizontal",
-                     strip.background = element_blank())
+                              fill = nhser_name, size = cases)) +
+  geom_jitter(pch = 21) +
+  facet_rep_wrap(. ~ week_infection, ncol = 2, repeat.tick.labels = "all") + 
+  scale_fill_brewer("", palette = "Set1") +
+  xlab("Proportion SGTF") +
+  ylab("Mean reproduction number") +
+  theme_cowplot(font_size = 10) +
+  geom_hline(yintercept = 1, linetype = "dashed") +
+  labs(size = paste("Cases")) +
+  guides(fill = guide_legend(title = "NHS region", ncol = 1), 
+         size = guide_legend(ncol = 1)) +
+  theme(legend.position = c(0.6, 0.125),
+        legend.direction = "vertical",
+        legend.box = "horizontal",
+        strip.background = element_blank())
 
 saveRDS(plot, here("output", "rt_sgtf_scatter.rds"))
 ggsave(here("output", "rt_sgtf_scatter.pdf"), plot, height = 7, width = 7) 
