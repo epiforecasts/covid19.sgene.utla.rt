0cc53a07a8b294aa2194169086b487360d9f2da8
seabbs <s.e.abbott12@gmail.com>
2021-02-01 22:43:26 +0000

    save baseline, fix results, add numeric effect

--- R/combine_severity.r
+++ R/combine_severity.r
@@ -3,6 +3,7 @@ library(dplyr)
 library(tidyr)
 library(here)
 library(purrr)
+library(brms)
 
 # Read in results ---------------------------------------------------------
 convolution_severity <-  readRDS(here("output", "convolution_severity.rds")) %>% 
@@ -28,9 +29,9 @@ saveRDS(lagged_score, here("data", "severity_models_scored.rds"))
 # Join results ------------------------------------------------------------
 severity <- convolution_severity %>% 
   bind_rows(lagged_severity) %>% 
-  select(-conv, -variant_effect_q) %>% 
+  select(-conv) %>% 
   select(target, loc, convolution, effect_type, effect = variant_effect, 
-         everything()) %>% 
+         effect_numeric = variant_effect_q, everything()) %>% 
   mutate(convolution = factor(convolution,
                               levels = c("global lag", "global convolution",
                                          "local convolution")))
@@ -49,4 +50,20 @@ lagged_severity_data <- severity %>%
   unique()
 
 saveRDS(lagged_severity_data, here("data", "lagged_severity_data.rds"))
+
+# Save baseline ratio -----------------------------------------------------
+baseline_severity_ratio <- severity %>%
+  mutate(baseline = map_chr(fit, function(x) {
+    samples <- posterior_samples(x, "b_Intercept")
+    q <- samples[, "b_Intercept"]
+    q <- exp(q)
+    q <- quantile(q, c(0.025, 0.5, 0.975))
+    q <- signif(q, 2)
+    q <- paste0(q[2]," (", q[1], ", ", q[3], ")")
+    return(q)
+  })) %>% 
+  select(-fit, -data, -loo) %>% 
+  arrange(loc, effect_type, convolution)
+
+saveRDS(baseline_severity_ratio, here("data", "baseline_severity_ratio.rds"))
 
