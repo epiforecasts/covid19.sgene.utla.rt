9546839194bbc8d9342b21cab1e09ae1a21479b8
seabbs <s.e.abbott12@gmail.com>
2021-06-24 23:02:17 +0000

    playing around complete

--- R/fit_models.r
+++ R/fit_models.r
@@ -13,7 +13,30 @@ utla_rt_with_covariates <-
   filter(week_infection > "2020-10-01") %>%
   mutate(tier = if_else(tier == "none", "_none", tier))
 
-# Add custom family -------------------------------------------------------
+# Example non-linear model ------------------------------------------------
+model <- bf(
+  rt_mean | se(rt_sd, sigma = TRUE) ~ (1 + var) * exp(logRt),
+  var ~ 0 + prop_sgtf,
+  logRt ~ 1,
+  family = student(),
+  nl = TRUE
+  # this is the change that allows future fancy stuff
+  # but as it stands tries to do a dot product :( (may need to open an issue)
+  #loop = FALSE
+)
+
+priors <- c(prior(normal(0, 0.5), nlpar = "var"),
+            prior(student_t(3, 0, 0.5), nlpar = "logRt"))
+
+fit <- brm(
+  model,
+  data = utla_rt_with_covariates %>%
+   rename(rt_mean = rt_mean_long_gt,
+          rt_sd = rt_sd_long_gt),
+  prior = priors,
+  control = list(adapt_delta = 0.95))
+
+# Define custom family ------------------------------------------------
 add_var_student <- custom_family(
   "add_var_student", dpars = c("mu", "sigma", "nu", "alpha"),
   links = c("log", "identity", "identity", "identity"),
